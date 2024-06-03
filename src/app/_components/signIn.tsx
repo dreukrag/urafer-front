63bb923ecb4c70cc1cf42872ab301953bb5f8755
Filipe <filipegroh@gmail.com>
2024-06-03 12:01:54 -0300

    - component to just handle the sign in process

--- src/app/_components/signIn.tsx
+++ src/app/_components/signIn.tsx
@@ -1,10 +1,4 @@
-import { signIn, signOut, auth } from "../auth";
-import clientPromise from "../db";
-
-const signOutHandler = async () => {
-  "use server";
-  await signOut({ redirectTo: "/" });
-};
+import { signIn } from "../auth";
 
 const signInHandler = async () => {
   "use server";
@@ -12,43 +6,8 @@ const signInHandler = async () => {
 };
 
 export const SignIn = async () => {
-  const session = await auth();
-
-  let content;
-
-  if (session?.user) {
-    try {
-      const client = await clientPromise;
-      const db = client.db("uranium_fever_db");
-      const coll = db.collection("uf_sessions");
-
-      await coll.insertOne({ sessionUser: session });
-    } catch (err) {
-      console.log("Error :C ", err);
-    }
-
-    content = (
-      <form action={signOutHandler}>
-        <button
-          className="group w-fit flex gap-4 pl-4 pr-4 items-center h-full"
-          type="submit"
-        >
-          <div className="flex flex-col gap-0.5 items-baseline">
-            <p className="text-textPrimary text-base font-bold group-hover:underline">
-              User Name here
-            </p>
-            <p className="text-textPrimary text-sm group-hover:underline">
-              User character here
-            </p>
-          </div>
-          <div className="rounded-full bg-textPrimary w-12 h-12 flex justify-center items-center">
-            <span>UN</span>
-          </div>
-        </button>
-      </form>
-    );
-  } else {
-    content = (
+  return (
+    <div className="bg-backgroundDark h-16 flex w-auto ml-auto">
       <form
         className="w-fit group flex gap-4 pl-4 pr-4 items-center h-full"
         action={signInHandler}
@@ -57,10 +16,6 @@ export const SignIn = async () => {
           <img alt="Sign in with Google" src="/web_dark_rd_SI.svg" />
         </button>
       </form>
-    );
-  }
-
-  return (
-    <div className="bg-backgroundDark h-16 flex w-auto ml-auto">{content}</div>
+    </div>
   );
 };
