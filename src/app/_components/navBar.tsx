2a253c1b2d63366d8c9eddbdebc8ac683b796b74
Filipe <filipegroh@gmail.com>
2024-06-03 12:01:18 -0300

    - navbar handling wheter user is logged in or not

--- src/app/_components/navBar.tsx
+++ src/app/_components/navBar.tsx
@@ -1,41 +1,14 @@
-import Link from "next/link";
-
 import { SignIn } from "@/app/_components/signIn";
 import { auth } from "../auth";
-interface iFancyLink {
-  children: React.ReactNode;
-  href: string;
-  className?: string;
-  disabled?: boolean;
-}
-
-const FancyLink = ({ children, href, className, disabled }: iFancyLink) => {
-  return (
-    <Link
-      className={[
-        "hover:underline  text-2xl flex gap-2 items-center",
-        className,
-        disabled
-          ? "text-textPrimaryDesaturated pointer-events-none cursor-not-allowed"
-          : "text-textPrimary",
-      ].join(" ")}
-      href={href}
-    >
-      {children}{" "}
-      <div
-        className={[
-          " h-9 w-0.5",
-          disabled ? "bg-textPrimaryDesaturated" : "bg-textPrimary",
-        ].join(" ")}
-      />
-    </Link>
-  );
-};
+import { FancyLink } from "./FancyLink";
+import { LoggedIn } from "./loggedIn";
 
 export const Navbar = async () => {
   const session = await auth();
 
-  const isPlayDisabled = !Boolean(session?.user);
+  const isSignedIn = Boolean(session?.user);
+
+  const isPlayDisabled = !isSignedIn;
 
   return (
     <div className="bg-backgroundDarkSecondary w-full h-16 flex gap-2 pl-4">
@@ -45,8 +18,11 @@ export const Navbar = async () => {
       <FancyLink disabled={isPlayDisabled} href="/play">
         Play
       </FancyLink>
+      <FancyLink disabled={isPlayDisabled} href="/play/setup">
+        Setup
+      </FancyLink>
 
-      <SignIn />
+      {isSignedIn ? <LoggedIn /> : <SignIn />}
     </div>
   );
 };
