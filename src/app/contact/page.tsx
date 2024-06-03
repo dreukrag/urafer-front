c192613ed70882f67276323a05c32b66195f30f2
Filipe <filipegroh@gmail.com>
2024-06-03 12:02:16 -0300

    - netter contact page, still WIP

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import { DefaultButton } from "../_components/buttons";
+
 export default async function Contact() {
   return (
     <main className="flex flex-col justify-center p-24 text-textPrimary">
@@ -19,9 +21,7 @@ export default async function Contact() {
           <input className="border-b-4 rounded-sm bg-backgroundSecondary border-background focus:border-backgroundDarkSecondary focus:outline focus:outline-offset-2 focus:outline-backgroundDarkSecondary focus:outline-2 w-1/2"></input>
           <label>Your message</label>
           <textarea className="border-b-4 rounded-sm outline outline-1 outline-offset-2 min-h-16 outline-backgroundSecondary bg-backgroundSecondary border-background focus:border-backgroundDarkSecondary focus:outline  focus:outline-backgroundDarkSecondary focus:outline-2 "></textarea>
-          <button className="border-textPrimary border-2 rounded-lg w-fit px-4 mx-auto text-textPrimary hover:bg-textPrimary hover:text-backgroundSecondary">
-            Send message
-          </button>
+          <DefaultButton extendClassName className="mx-auto">Send message</DefaultButton>
         </form>
       </div>
       <p className="mt-4 w-full text-base">
