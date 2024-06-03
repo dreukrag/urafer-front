d9916c73bbea30c1234d74aca8a34d0e3ce5e8e5
Filipe <filipegroh@gmail.com>
2024-06-03 12:02:37 -0300

    - Real basica play page and layout

--- src/app/play/layout.tsx
+++ src/app/play/layout.tsx
@@ -1,15 +1,22 @@
-export default function PlayLayout({
+import { redirect } from "next/navigation";
+import { auth } from "../auth";
+
+export default async function PlayLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const hasSession = await auth();
+
+  if (Boolean(hasSession?.user) === false) {
+    redirect("/");
+  }
+
   return (
-    <section>
+    <>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <nav>left</nav>
 
       {children}
-      <nav>right</nav>
-    </section>
+    </>
   );
 }
--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2,10 +2,12 @@
 export default function PlayPage() {
   return (
     <div>
+      <div>left</div>
       <h1>
         This is the play area. There should be stuff like a command prompt and a
         huge wall of text describing the current location and shit like that
       </h1>
+      <div>right</div>
     </div>
   );
 }
